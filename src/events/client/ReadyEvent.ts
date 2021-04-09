bd81ae76d1b29979b046cadaf677fca757d01521
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2021-04-08 21:21:13 -0700

    rename misc directory to info

--- src/events/client/ReadyEvent.ts
+++ src/events/client/ReadyEvent.ts
@@ -4,6 +4,15 @@ const ReadyEvent: Event = {
   name: "ready",
   run(client, message, args) {
     console.log("ready");
+
+    client.user?.setPresence({
+      activity: {
+        type: "PLAYING",
+        name: "an!help",
+      },
+
+      status: "online",
+    });
   },
 };
 
