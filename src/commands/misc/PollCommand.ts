e7d13b4e3dad3dabf498b23b05758e82dce6f7a8
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2021-04-09 21:32:53 -0700

    start of poll command

--- src/commands/misc/PollCommand.ts
+++ src/commands/misc/PollCommand.ts
@@ -5,7 +5,19 @@ const MathCommand: Command = {
   description: "Creates a poll",
   category: "misc",
   aliases: ["vote", "ballot"],
-  run(client, message, args) {},
+  run(client, message, args) {
+    if (!message.member?.permissions.has("MANAGE_CHANNELS")) {
+      return message.channel.send(
+        client.embed(
+          {
+            description:
+              "You need to have the **Manage Channels** permission!s",
+          },
+          message
+        )
+      );
+    }
+  },
 };
 
 export = MathCommand;
